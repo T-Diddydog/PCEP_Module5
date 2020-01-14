8d5326d194b52c9eec3f4cf1870ba2892f9cbc25
T-Diddydog <twalkski@gmail.com>
2020-01-14 19:48:10 +1100

    Module5 file now in git

---
+++ import_module_technique_2.py
@@ -0,0 +1,15 @@
+from math import sin, pi        # line 01
+
+print(sin(pi/2))                # line 03
+
+pi = 3.14                       # line 05
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print(sin(pi/2))                # line 13
+
+
